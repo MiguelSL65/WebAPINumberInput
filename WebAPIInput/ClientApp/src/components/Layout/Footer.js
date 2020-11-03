9d3c698a86bf33773e81826b4a8d6b71b68a14c3
Miguel Lemos <mimiguelsl65@gmail.com>
2020-11-03 22:21:57 +0000

    added update on footer

--- WebAPIInput/ClientApp/src/components/Layout/Footer.js
+++ WebAPIInput/ClientApp/src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
 
     return(
         <footer>
-            <p id='footertext'>Created by Miguel Lemos @ 11/2020 <a id='footerlink' href='https://github.com/MiguelSL65/NumberInputWebAPI'>Github repo</a></p>
+            <p id='footertext'>Developed by Miguel Lemos <a id='footerlink' href='https://github.com/MiguelSL65/NumberInputWebAPI'>Github repo</a></p>
         </footer>
     )
 }
